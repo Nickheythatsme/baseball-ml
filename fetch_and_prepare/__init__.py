5b49e9fb23617ae63b3e55ea250b9d1e96b85a19
Nicholas Grout <njgrout@gmail.com>
2019-06-18 23:48:37 -0700

    setting up boiler plate 	modified:   __init__.py 	modified:   data_reader.py 	modified:   player.py 	deleted:    read_data.py

--- fetch_and_prepare/__init__.py
+++ fetch_and_prepare/__init__.py
@@ -1,1 +1,3 @@
 from .fetch_data import FetchData
+from .data_reader import DataReader
+from .player import Player
---
+++ fetch_and_prepare/player.py
@@ -0,0 +1,5 @@
+from .data_reader import DataReader
+
+class Player(DataReader):
+    def __init__(self):
+        self.name = ''
